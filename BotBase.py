d006716bd5c5c26e2e3d5e8d7a1d1dc4d3e96ee3
mohamedtalaatLU <39444269+mohamedtalaatLU@users.noreply.github.com>
2018-06-19 15:18:29 +0200

    Add files via upload

---
+++ BotBase.py
@@ -0,0 +1,36 @@
+s = """
+import discord
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "*")
+
+@client.event
+async def on_ready():
+    print("boton is ready")
+    SayInput = input("Enter content to say ")
+    await client.send_message(channel, "%s" % SayInput)
+    await client.change_presence(game=discord.Game(name='LEGO Universe'))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("hello"):
+        await client.send_message("welcome")
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+client.run("NDUzNjI0MzY2MjE1OTg3MjEw.DfhqVg.SHV4scbnvxlR83Pw-zmXiVPzZKU")
+"""
---
+++ Pyboter.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import blank
+
+
+def CreateBot():
+    f = open("bot.py","w")
+    f.write(blank.s)
+
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        return
+    
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Creat \n a \n Bot"
+        self.hi_there["command"] = CreateBot()
+        self.hi_there.pack(side="top")
+        self.hi_there.size()
+        self.quit = tk.Button(self, text="QUIT", fg="red", bg = "yellow", command=root.destroy)
+        self.quit.pack(side="bottom")
+
+
+
+
+
+root = tk.Tk()
+app = App()
+app = app.master
+app.minsize(500,500)
+app.title("PyBoter")
+app.mainloop()
