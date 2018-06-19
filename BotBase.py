c69b5659f61157b97668b9baaca8c7af278ee521
mohamedtalaatLU <39444269+mohamedtalaatLU@users.noreply.github.com>
2018-06-19 19:01:51 +0200

    Update BotBase.py

--- BotBase.py
+++ BotBase.py
@@ -11,8 +11,6 @@ client = commands.Bot(command_prefix = "*")
 @client.event
 async def on_ready():
     print("boton is ready")
-    SayInput = input("Enter content to say ")
-    await client.send_message(channel, "%s" % SayInput)
     await client.change_presence(game=discord.Game(name='LEGO Universe'))
 
 @client.event
