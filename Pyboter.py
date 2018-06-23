5b518143406d4f7a7c56a00f19303fe003c4fb7a
mohamedtalaatLU <39444269+mohamedtalaatLU@users.noreply.github.com>
2018-06-23 12:03:16 +0200

    Update Pyboter.py
    Thanks to noud02

--- Pyboter.py
+++ Pyboter.py
@@ -8,7 +8,6 @@ class App(tk.Frame):
         self.pack()
         self.create_widgets()
         return
-    
 
     def create_widgets(self):
         self.widg = tk.Button(self)
@@ -21,14 +20,17 @@ class App(tk.Frame):
         self.butt["text"] = "Enable anti-spam"
         self.butt["command"] = None #this is not implemented yet
         self.butt.pack(side="right")
-        self.butt1 = tk.Button(self, text="DB based bot", command = Based = True)
-        if Based == True:
-            mstr = Tk()
-            Label(mstr, text = "SQL host or IP").grid(row = 3)
-            e = Entry(mstr)
-            e.grid(row = 3 , column =, 3
+
+        def show_box():
+            mstr = tk.Tk()
+            tk.Label(mstr, text = "SQL host or IP").grid(row = 3)
+            e = tk.Entry(mstr)
+            e.grid(row = 3 , column = 3)#thanks to noud02 the credits of creating this function goes to him
+
+        self.butt1 = tk.Button(self, text="DB based bot", command=show_box)
+
         self.butt1.pack(side = "left")
-        self.butt2 = tk.button(self, text ="Change game the bot is playing")
+        self.butt2 = tk.Button(self, text ="Change game the bot is playing")
         self.quit = tk.Button(self, text="QUIT", fg="red", bg = "yellow", command=root.destroy)
         self.quit.pack(side="bottom")
 
